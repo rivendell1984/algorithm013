ba4eb0b74df377271725e6dfeb17a651791f67cb
Liang HAN <rivendell1984@gmail.com>
2020-08-09 14:31:13 +0800

    submit week 2 homework

---
+++ Week_02/valid-anagram.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.nio.charset.*;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        boolean isTrue = solution.isAnagram("anagram", "aagaram");
+    }
+
+
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length != t.length) {
+            return false;
+        }
+
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for (int count : counter) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
